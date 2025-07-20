effd44ffc7fc1ab7b4ef78fb7ea451067129ac09
rnpublic88628867 <rnpublicschool7761@gmail.com>
2025-07-21 00:16:27 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,13 +7,13 @@ API_ID = int(environ.get("API_ID", "28585138"))
 API_HASH = environ.get("API_HASH", "74b7afde7d9a546db3451cf4689f4e14")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7431004177"))
-CREDIT = environ.get("CREDIT", "🐲🐉 𝗗𝗥. 𝗞𝗜𝗟𝗟𝗬 💘💔")
+OWNER = int(environ.get("OWNER", "7181075926"))
+CREDIT = environ.get("CREDIT", "@DOCTOR_JB")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6148865829').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7431004177').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7431004177,6148865829').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
